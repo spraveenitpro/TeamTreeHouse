339defd4be4b0aeb07c1ad37f47f8c228d55b115
spraveenitpro <spraveenitpro@gmail.com>
2014-07-15 16:46:21 +0530

    Started Java script lessons

---
+++ JS foundations/variables/myscript.js
@@ -0,0 +1,79 @@
+ 
+/*************************************************************/
+/* JavaScript Foundations */
+/*************************************************************/
+
+
+ // var color = "red"; 
+
+ // var myDiv = document.getElementById('myDiv');
+ // myDiv.style.background="black";
+ // myDiv.style.color="white";
+
+ //Valid Variables
+
+ // var car = "Toyota";
+ // var Color = "blue";
+ // var _myVariable = "something";
+ // var $specialName = 1;
+ // var a58389 = "what is this"; 
+
+ //Invalid Variables
+
+ //var 3colors = "Green";
+ //var winning% = "hello";
+ //var hello-world = "John Doe";
+ //var @you = "awesome";
+
+ //var continue = true;
+
+ // var myVar;
+ // console.log(typeof myVar === "undefined");
+ // console.log(myVar === undefined);
+
+ // var x = null;
+ 
+ // if(myVar == null) {
+ // 	console.log("If");
+ // } else {
+ // 	console.log("Else");
+ // }
+
+ //Scope
+
+ // var world = "World!";
+ 
+ // function sayHello () {
+ // 	var hello = "Hello ";
+
+ // 	function inner() {
+ // 		var extra = "There is more !"; 
+ // 		console.log(hello + world + extra);
+ // 	}
+
+ // 	inner();
+ 	 
+ // }
+
+ // sayHello();
+
+ // console.log("world Outside sayHello(): ", world);
+ //console.log("world Outside sayHello(): ", hello);
+
+ //Shadowing
+
+ // var myColor = "blue";
+ // console.log("myColor before myFunc()", myColor);
+
+ // function myFunc () {
+ // 	var myColor = "yellow";
+ // 	myNumber = 42;
+ // 	console.log("myColor before myFunc()", myColor);
+ // }
+
+ // myFunc();
+
+ // console.log("myColor after myFunc()", myColor);
+ // console.log("myNumber after myFunc()", myNumber);
+
+ 
